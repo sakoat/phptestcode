e415dd21f67fd745911cf85e511c7600f4821aa4
sakoat07 <sakoatcse@gmail.com>
2022-12-11 22:21:58 +0600

    update filtering string content using str_replace

--- filter_text_content.php
+++ filter_text_content.php
@@ -8,6 +8,7 @@ Location data{ "lat":345666, "lang":78899}
 ';
 $trimed_str = trim($str);
 $final_string = preg_replace("/[ \\r\n\/\s]*$/", "" ,$trimed_str);
+//$text = str_replace( array("\r", "\n", "\t", "/", "<br>"), "", $final_string );
 $text = preg_replace( "/<br>|\n/", "", $final_string );
 
 echo $text;
